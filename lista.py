ed9a81226424d94942794719a127d646ed8eb816
Fernanda <fernandamarquesluizgaspar@gmail.com>
2025-08-01 09:02:31 -0300

    Quase la

--- lista.py
+++ lista.py
@@ -1,5 +1,42 @@
 afazeres = []
-adicionar = input("Deseja adicionar um item a sua lista? ").upper
-if adicionar == "SIM":
-    item = input("Qual item você deseja adicionar? ")
+
+while True:
+
+    print("""
+            ___________________________________
+            *            AFAZERES             *
+            -----------------------------------
+            *                                 *
+            *   1 - Inserir tarefa            *
+            *   2 - Excluir tarefa            *
+            *   3 - Concluir tarefa           *
+            *   4 - Listar tarefas            *
+            *   0 - SAIR                      *
+            ___________________________________
+    """)
+
+    acao = int(input("O que você deseja fazer? "))
+
+    if acao == 1:
+        item = input("O que você deseja adicionar? ").upper()
+        afazeres.append(item)
+
+    elif acao == 2:
+        item = input("O que você deseja retirar? ").upper()
+        afazeres.remove(item)
+
+    elif acao == 3:
+        concluir = input("Qual item você deseja marcar como conlcluído? ").upper()
+        for item in afazeres:
+            if item == concluir:
+                print(*f"{item} ✓")
+
+    elif acao == 4: 
+        for item in afazeres:
+            print(*item)
     
+    elif acao == 0:
+        break
+
+    else:
+        acao = input("O que você deseja fazer? ")
